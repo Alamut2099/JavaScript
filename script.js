aee66fddc280968d501df0982991064ed9346448
Ibroximjon <abdujalilovjamshid0798@gmail.com>
2022-10-13 20:17:37 +0900

    math consol. log lesson

---
+++ script.js
@@ -0,0 +1,122 @@
+// a = 10
+// b = 3
+// c = a/b
+
+
+// d = 9.11874893803584
+
+// console.log(c)     (javobni togri qoldigi bn chiqarib berish uchun.)
+
+// console.log(Number.parseInt(c))  (sonlarni butun qismini ozini olib berish uchun)
+// console.log(Number.parseFloat(c))  (sonlarni qoldigi bn )(kasri bn 1/5 va hokazo) defolt holatida chiqarb ebrish)
+
+
+// console.log(d.toFixed(2))
+// console.log(typeof d.toFixed(2))  "string yokimsonligini aniqlash uchun stringlar qoshtirnoq ichida boladi ular bn matematika ishlamedi garchi son korinisida bolgani bilan ham"
+// // console.log(d)
+
+
+
+
+// Maths
+// 1.Math.round "javoblarni butun songa yaxlitlash (yani 1200lik narsani 1000 deyishdek 1600likni 2000 deyishdek gap)"
+// a = 10
+// b = 3
+// c = a/b
+// console.log(Math.round(c))
+
+
+//2. Math.pow  " biror bir sonni darajaga ko'tarish"
+// console.log(math.pow(5, 3 "ya'ni 5ni 3-darajasi"))
+
+
+// "3." Math.sqrt = "sonlarni ildiz ostiga olish"
+// console.log(Math.sqrt(25)), console.log(Math.sqrt(25))
+
+
+// "4."   Math.cbrt "Kub ildiz ostiga olish(biror bir sonni 3-darajasini ildizga olish)"
+// console.log(Math.cbrt(125))  "125ni kubildiz ostiga olsak javob 5"
+
+
+
+// "5"   Math.abs "Moduldan chiqarish(javoblar manfiy bolsa ham musbatda chiqarb beradi minusni olibtashedi)"
+// console.log(Math.abs(25-32)) "javob 7ni ozi (NO-)"
+
+
+// "6"    Math.ceil "Kasr sonini yuqoriga yaxlitlash (umumiy yuqoriga yaxlitlash yani 1100lik narsani 2000 deyishdek gap)"
+// console.log(Math.ceil(1.2))
+
+
+// "7"    Math.floor (sonlarni faqat pastga qarab yaxlitleydi 1800 bolsa ham 1000 ga yaxlitlagandek)
+
+
+// `1`
+//  a = 6     
+//  p = 4*a
+// console.log(p)
+
+
+
+
+// a = 8       ` 2`
+// s = a**2
+// console.log(s)
+
+
+
+
+// a = 4       `  3`
+// b = 5
+// s = a*b
+// console.log(s)
+// p = 2*(a+b)
+// console.log(p)
+
+
+
+// p = 3.14      ` 4`
+// d = 7
+// l = p*d
+// console.log(l)
+
+
+
+`5`
+// a = 6       
+// v = a**3 
+
+// s = 6*a**2
+// console.log(v,s)
+
+
+
+
+// a = 3          `  6`
+// b = 9
+// c = 2
+// v = a*b*c
+
+// s = 2*(a*b+b*c+a*c)
+// console.log(v,s)
+
+
+
+
+
+// r = 18           ` 7`
+// p = 3.14
+// l = 2*p*r
+// s = p*r**2
+// console.log(l,s)
+
+
+               
+// a = 2      ` 8 `     
+// b = 10
+// console.log((a+b)/2)
+
+
+// `9`
+// a = 5
+// b = 2
+// console.log(Math.sqrt(5*2))
---
+++ script2.js
@@ -0,0 +1,103 @@
+// `1`
+//  a = 6     
+//  p = 4*a
+// console.log(p)
+
+
+// `2`
+// a = 8
+// s = a**2
+// console.log(s)
+
+
+// `3`
+// a = 4
+// b = 5
+// s = a*b
+// p = 2*(a+b)
+// console.log(s,p)
+
+
+// "4"
+// p = 3.14
+// d = 7
+// l = p*d
+// console.log(l)
+
+
+// `5`
+// a = 6
+// v = a**3
+// s = 6*a**2
+// console.log(v,s)
+
+
+// `6`
+// a = 3
+// b = 9
+// c = 2
+// v = a*b*c
+// s = 2*(a*b+b*c+a*c)
+// console.log(v,s)
+
+
+// `7`
+// r = 18
+// p = 3.14
+// l = 2*p*r
+// s = p*r**2
+// console.log(l,s)
+
+
+// `8`
+// a = 2
+// b = 10
+// console.log((a+b)/2)
+
+
+// `9`
+// a = 5
+// b = 2
+// console.log(Math.sqrt(a*b))
+
+
+// `10`
+// a =8
+// b = 9
+// c = a+b
+// d = a*b
+// e = a**2
+// f = b**2
+// console.log(c,d,e,f)
+
+// `yoki`
+// console.log((8+9),(8*9),(8**2),(9**2))
+
+
+
+
+
+
+
+
+
+// `12`
+// a = 25
+// b = 36
+// c = a**2+b**2
+// console.log(Math.sqrt(c))          
+// console.log(Math.sqrt (a**2+b**2))                  
+
+
+// `13`
+// r1 =15
+// r2 =13
+// p = 3.14
+// s1 = p*r1
+// s2 = p*r2
+// s3 = p*(r1-r2)
+// console.log(s1,s2,s3)
+
+
+
+// 20 ta masala tuzish
